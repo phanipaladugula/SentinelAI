319c7b2bd25925d7ef42e56185c4f63e635310e3
Phani <phanipaladugulaa@gmail.com>
2026-03-12 20:04:13 +0530

    fix: reset migrations and recreate cameras table

--- Backend/migrations/20260312143235_create_cameras_table.up.sql
+++ Backend/migrations/20260312143235_create_cameras_table.up.sql
@@ -1,8 +1,10 @@
-CREATE TABLE cameras(
-    id UUID PRIMARY KEY,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE cameras (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
     rtsp_url TEXT NOT NULL,
     location TEXT,
     status TEXT DEFAULT 'offline',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
